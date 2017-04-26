27884c575cae2cdc49b6987e46d1206d0ec34bac
mumingv <mumingv@163.com>
2017-04-27 02:04:40 +0800

    tp5自动化模块搭建

--- imooc_o2o/application/build.php
+++ imooc_o2o/application/build.php
@@ -14,12 +14,27 @@ return [
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
+    /*
     'demo'     => [
         '__file__'   => ['common.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
         'controller' => ['Index', 'Test', 'UserType'],
         'model'      => ['User', 'UserType'],
         'view'       => ['index/index'],
+    ],
+    */
+    'common' => [
+        '__dir__' => ['model'],
+        'model' => ['Category', 'Admin'],
+    ],
+    'admin' => [
+        '__dir__' => ['controller', 'view'],
+        'controller' => ['Index'],
+        'view'       => ['index/index'],
+    ],
+    'api' => [
+        '__dir__' => ['controller', 'view'],
+        'controller' => ['Index', 'Image'],
     ],
     // 其他更多的模块定义
 ];
