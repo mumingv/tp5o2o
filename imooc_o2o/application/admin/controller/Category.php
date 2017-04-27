7ad32be45081b9a2d54131b9026ebdc50095f65e
mumingv <mumingv@163.com>
2017-04-27 23:11:14 +0800

    5-2 tp5-validate在实际项目中的应用

--- imooc_o2o/application/admin/controller/Category.php
+++ imooc_o2o/application/admin/controller/Category.php
@@ -17,6 +17,15 @@ class Category extends Controller
         // 获取数据表单数据的几种形式
         //print_r($_POST);  // 方法一：原生方式
         //print_r(input('post.'));  // 方法二：推荐
-        print_r(request()->post());  // 方法三：推荐
+        //print_r(request()->post());  // 方法三：推荐
+        $data = input('post.');
+        $data['status'] = 10;
+        $validate = validate('Category');
+        //if (!$validate->check($data)) {
+        if (!$validate->scene('add')->check($data)) {
+            $this->error($validate->getError());     
+        }
+
+        // 把$data提交给model层
     }
 }
